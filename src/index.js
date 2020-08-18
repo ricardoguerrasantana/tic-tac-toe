eaeedaad85274b92444086e4ce2e24a722959e85
Ricarr <37304218+Repositorio-Ric@users.noreply.github.com>
2020-08-18 20:52:25 +1000

    Show whose turn is or the winner

--- src/index.js
+++ src/index.js
@@ -52,8 +52,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = `Next player: 
-        ${this.state.xIsNext ? 'X' : 'O'}`;
+        const squares = this.state.squares.slice();
+        const winner = calculateWinner(squares);
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player: 
+            ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
 
         return (
             <div>
