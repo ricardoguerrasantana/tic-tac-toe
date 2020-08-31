ccbc489bcaf053d3c26ea218a16063a055b426af
Ricarr <37304218+Repositorio-Ric@users.noreply.github.com>
2020-09-01 02:13:34 +1000

    Updates

---
+++ precache-manifest.e66fa3e2434a2e1ed68e51113ebccd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1276abb8340bbeea017d36e261bc9d2b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "24fafc26bf492f32f038",
+    "url": "/tic-tac-toe/static/css/main.fda19b3a.chunk.css"
+  },
+  {
+    "revision": "3e8f8f95d427a00b9b8c",
+    "url": "/tic-tac-toe/static/js/2.02200332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.02200332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fafc26bf492f32f038",
+    "url": "/tic-tac-toe/static/js/main.10eccbff.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
